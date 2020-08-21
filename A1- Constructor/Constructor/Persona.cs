4c369dbbc1aac99d1e6f31f1a3c8051ba75ad88c
Lunaysa <60144289+Lunaysa@users.noreply.github.com>
2020-08-21 08:37:34 -0500

    Add files via upload

---
+++ A1- Constructor/Constructor/Persona.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructor
+{
+    class Persona
+    {
+        private string nombre;
+        private int edad;
+        private int cedula;
+
+        public string Nombre { get => nombre;}
+        public int Edad { get => edad; }
+        public int Cedula { get => cedula; }
+
+        //Constructor
+        
+
+        public Persona(string nombre, int edad, int cedula)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.cedula = cedula;
+        }
+
+        public string MostrarPersona()
+        {
+            string persona = "Nombre: " + nombre + " Cedula: " + cedula +  " Edad: " + edad;
+            return persona;
+        }
+    }
+}
---
+++ A1- Constructor/Constructor/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            List<Persona> listaPersonas = new List<Persona>();
+
+            for (int i = 0; i < 1 ; i++)
+            {
+                Console.WriteLine("Ingrese nombre");
+                string nombre = Console.ReadLine();
+                Console.WriteLine("Ingrese cedula");
+                int cedula = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese edad");
+                int edad = int.Parse(Console.ReadLine());
+                Persona p = new Persona(nombre, edad, cedula);
+                listaPersonas.Add(p);
+
+            }
+
+            for (int i = 0; i < listaPersonas.Count; i++)
+            {
+                Console.WriteLine(listaPersonas[i].MostrarPersona());
+            }
+            
+            
+           
+
+
+            
+          
+
+
+        }
+    }
+}
